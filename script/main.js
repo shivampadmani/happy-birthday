b7fbd06f4c98487109d36585650b9e985b1aff0e
Shivam Padmani <56795691+shivampadmani@users.noreply.github.com>
2025-10-18 17:53:37 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -220,12 +220,18 @@ const animationTimeline = () => {
       },
       "-=2"
     )
+    // .from(".hat", 0.5, {
+    //   x: -100,
+    //   y: 350,
+    //   rotation: -180,
+    //   opacity: 0
+    // })
     .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
-      rotation: -180,
-      opacity: 0
-    })
+  x: -50,        // Changed from -100 to -50 (moves it to the right)
+  y: 350,
+  rotation: -180,
+  opacity: 0
+})
     .staggerFrom(
       ".wish-hbd span",
       0.7,
